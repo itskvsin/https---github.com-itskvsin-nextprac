6a86249dbf75fdc8ec3d455c037296fa17c873ce
Kevin <kevinsolankistudy@gmail.com>
2025-06-23 18:37:41 +0530

    added the type to the text of the button

--- src/components/MovingBorder.tsx
+++ src/components/MovingBorder.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 import { Button } from "./ui/moving-border";
 
-export function MovingBorderDemo({text}) {
+interface movingBorder{
+  text: string,
+}
+
+export function MovingBorderDemo({text} : movingBorder) {
   return (
     <div>
       <Button
