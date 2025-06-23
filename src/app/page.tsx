f3c04a023f62b1876bc55d89013d77f4b5033888
Kevin <kevinsolankistudy@gmail.com>
2025-06-23 17:48:20 +0530

    added carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+import Carousels from "@/components/Carousels";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
+  const images = [
+    "https://images.unsplash.com/photo-1749587452499-ea1fd591e63f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1750008267598-7f68e1a25ab8?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8",
+    "https://images.unsplash.com/photo-1749813482475-3c12a8c4a5bd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D",
+  ];
   return (
     <main className="h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2] ">
-      <HeroSection /> 
+      <HeroSection />
       <FeaturedCourses />
+      {/* <Carousels /> */}
     </main>
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
     //   <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
